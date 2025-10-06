69dfdd9fb3418f112f7a63ca11173857aecf0fb9
Nathan Vasse <nathan.vasse@gmail.com>
2025-10-06 11:33:11 +0200

    ✨(back) extend URL routing for operators and organizations
    Added new routes for OperatorViewSet, OrganizationViewSet,
    and their associated endpoints, including organization-specific
    service management. This enhances the API's structure to support
    nested organization and operator functionalities, improving overall
    access and management capabilities.

--- src/backend/core/urls.py
+++ src/backend/core/urls.py
@@ -3,7 +3,7 @@ URL configuration for core app.
 """
 
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from rest_framework.routers import DefaultRouter
 
@@ -12,6 +12,14 @@ from core.authentication.urls import urlpatterns as oidc_urls
 from .api.viewsets.config import ConfigView
 from .api.viewsets.lagaufre import LagaufreViewSet
 from .api.viewsets.service import ServiceLogoViewSet, ServiceViewSet
+from .api.viewsets.operator import OperatorViewSet
+from .api.viewsets.organization import OperatorOrganizationViewSet, OrganizationViewSet
+from .api.viewsets.service import (
+    OrganizationServiceSubscriptionViewSet,
+    OrganizationServiceViewSet,
+    ServiceLogoViewSet,
+    ServiceViewSet,
+)
 from .api.viewsets.user import UserViewSet
 
 # Create router and register viewsets
@@ -20,6 +28,19 @@ router.register(r"lagaufre", LagaufreViewSet, basename="lagaufre")
 router.register(r"services", ServiceViewSet)
 router.register(r"servicelogo", ServiceLogoViewSet, basename="servicelogo")
 router.register(r"users", UserViewSet, basename="user")
+router.register(r"operators", OperatorViewSet)
+router.register(r"organizations", OrganizationViewSet)
+
+operator_organization_router = DefaultRouter()
+operator_organization_router.register(r"organizations", OperatorOrganizationViewSet)
+
+organization_service_router = DefaultRouter()
+organization_service_router.register(r"services", OrganizationServiceViewSet)
+
+organization_service_subscription_router = DefaultRouter()
+organization_service_subscription_router.register(
+    r"subscription", OrganizationServiceSubscriptionViewSet
+)
 
 urlpatterns = [
     # Include all router URLs
@@ -30,6 +51,18 @@ urlpatterns = [
                 *router.urls,
                 *oidc_urls,
                 path("config/", ConfigView.as_view(), name="api-config"),
+                re_path(
+                    r"^operators/(?P<operator_id>[0-9a-z-]*)/",
+                    include(operator_organization_router.urls),
+                ),
+                re_path(
+                    r"^organizations/(?P<organization_id>[0-9a-z-]*)/",
+                    include(organization_service_router.urls),
+                ),
+                re_path(
+                    r"^organizations/(?P<organization_id>[0-9a-z-]*)/services/(?P<service_id>[0-9a-z-]*)/",
+                    include(organization_service_subscription_router.urls),
+                ),
             ]
         ),
     ),
