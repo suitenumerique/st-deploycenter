5e5e966262f2c3439991bbf9c695051ce9a551b1
Nathan Vasse <nathan.vasse@gmail.com>
2025-12-11 15:07:44 +0100

    ✨(front) add generic entitlements fields
    We want to be able to provide a map that describe which fields
    are controlled by which type of input. It will make easier the
    future services implementations and avoid too much work and
    duplication.

--- src/frontend/src/features/ui/components/service/ServiceBlock.tsx
+++ src/frontend/src/features/ui/components/service/ServiceBlock.tsx
@@ -1,12 +1,74 @@
-import { Organization, ServiceSubscription } from "@/features/api/Repository";
+import {
+  Entitlement,
+  Organization,
+  ServiceSubscription,
+} from "@/features/api/Repository";
 import { useTranslation } from "react-i18next";
 import { Service } from "@/features/api/Repository";
-import { Button, Switch, Tooltip, useModals } from "@openfun/cunningham-react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalSize,
+  Switch,
+  Tooltip,
+  useModal,
+  useModals,
+} from "@openfun/cunningham-react";
 import { Icon, IconSize } from "@gouvfr-lasuite/ui-kit";
 import { useEffect, useState } from "react";
 import { useMutationUpdateOrganizationServiceSubscription } from "@/hooks/useQueries";
 import { useOperatorContext } from "@/features/layouts/components/GlobalLayout";
 import { MutateOptions } from "@tanstack/react-query";
+import { ServiceAttribute } from "./ServiceAttribute";
+import prettyBytes from "pretty-bytes";
+import { StoragePickerEntitlementField } from "./entitlements/fields/StoragePickerEntitlementField";
+
+export type EntitlementFields = {
+  fields: Record<string, { type: string }>;
+};
+
+export type EntitlementsFields = Record<string, EntitlementFields>;
+
+/**
+ * This map defines the fields for the entitlements of a service block.
+ *
+ * It goes:
+ * {
+ *  <service_type>: {
+ *    entitlements: {
+ *      <entitlement_type>: {
+ *        fields: [
+ *          {
+ *            <field_name>: {
+ *              type: <field_type>,
+ *            },
+ *          },
+ *        ],
+ *      },
+ *    },
+ *  },
+ * }
+ *
+ */
+const ENTITLEMENTS_FIELDS: Record<
+  string,
+  {
+    entitlements: EntitlementsFields;
+  }
+> = {
+  drive: {
+    entitlements: {
+      drive_storage: {
+        fields: {
+          max_storage: {
+            type: "storage_picker",
+          },
+        },
+      },
+    },
+  },
+};
 
 export const useServiceBlock = (
   service: Service,
@@ -42,6 +104,9 @@ export const useServiceBlock = (
     return true;
   };
 
+  const entitlementsFields =
+    ENTITLEMENTS_FIELDS[service.type]?.entitlements || {};
+
   return {
     service,
     organization,
@@ -49,30 +114,25 @@ export const useServiceBlock = (
     setChecked,
     onChangeSubscription,
     canActivateSubscription,
+    entitlementsFields,
   };
 };
 
-export const ServiceBlock = ({
-  service,
-  organization,
-  checked,
-  setChecked,
-  onChangeSubscription,
-  content,
-  canActivateSubscription,
-  showGoto = true,
-}: {
+type ServiceBlockProps = {
   service: Service;
   organization: Organization;
   content?: React.ReactNode;
   showGoto?: boolean;
-} & ReturnType<typeof useServiceBlock>) => {
+  entitlementsFields?: EntitlementsFields;
+} & ReturnType<typeof useServiceBlock>;
+
+export const ServiceBlock = (props: ServiceBlockProps) => {
   const { t } = useTranslation();
   const modals = useModals();
   const isExternallyManaged =
-    service.operator_config?.externally_managed === true;
+    props.service.operator_config?.externally_managed === true;
 
-  const canSwitch = !isExternallyManaged && service.can_activate;
+  const canSwitch = !isExternallyManaged && props.service.can_activate;
 
   return (
     <div
@@ -82,50 +142,52 @@ export const ServiceBlock = ({
     >
       <div className="dc__service__block__header">
         <div className="dc__service__block__header__title">
-          {service.logo && (
+          {props.service.logo && (
             <div className="dc__service__block__header__logo">
-              <img src={service.logo} alt={service.name} />
+              <img src={props.service.logo} alt={props.service.name} />
             </div>
           )}
-          <div className="dc__service__block__header__name">{service.name}</div>
+          <div className="dc__service__block__header__name">
+            {props.service.name}
+          </div>
         </div>
         {canSwitch ? (
           <Switch
-            checked={checked}
+            checked={props.checked}
             onChange={async (e) => {
               if (e.target.checked) {
-                if (!(await canActivateSubscription())) {
+                if (!(await props.canActivateSubscription())) {
                   return;
                 }
                 const decision = await modals.confirmationModal();
                 if (decision === "yes") {
-                  onChangeSubscription(
+                  props.onChangeSubscription(
                     { is_active: true },
                     {
                       onError: () => {
-                        setChecked(false);
+                        props.setChecked(false);
                       },
                     }
                   );
-                  setChecked(true);
+                  props.setChecked(true);
                 }
               } else {
                 const decision = await modals.deleteConfirmationModal({
                   title: t("organizations.services.disable_modal.title"),
                   children: t("organizations.services.disable_modal.content", {
-                    service: service.name,
+                    service: props.service.name,
                   }),
                 });
                 if (decision === "delete") {
-                  onChangeSubscription(
+                  props.onChangeSubscription(
                     { is_active: false },
                     {
                       onError: () => {
-                        setChecked(true);
+                        props.setChecked(true);
                       },
                     }
                   );
-                  setChecked(false);
+                  props.setChecked(false);
                 }
               }
             }}
@@ -144,19 +206,19 @@ export const ServiceBlock = ({
               tabIndex={0}
               aria-label={t("organizations.services.externally_managed")}
             >
-              <Switch checked={checked} disabled={true} />
+              <Switch checked={props.checked} disabled={true} />
             </div>
           </Tooltip>
         )}
       </div>
       <div className="dc__service__block__body">
-        {!!service.description && (
+        {!!props.service.description && (
           <div className="dc__service__block__description">
-            {service.description}
+            {props.service.description}
           </div>
         )}
         <div className="dc__service__block__body__content">
-          {content}
+          {props.content}
           {/*
             <div className="dc__service__block__values">
               <div className="dc__service__block__values__item">
@@ -169,15 +231,18 @@ export const ServiceBlock = ({
               </div>
             </div>
             */}
-          {showGoto && service.url && (
+          {Object.keys(props.entitlementsFields).length > 0 && (
+            <ServiceBlockEntitlements {...props} />
+          )}
+          {props.showGoto && props.service.url && (
             <div className="dc__service__block__goto">
-              <a target="_blank" href={service.url}>
+              <a target="_blank" href={props.service.url}>
                 {t("organizations.services.goto")}
               </a>
               <Button
                 color="tertiary"
                 size="nano"
-                href={service.url}
+                href={props.service.url}
                 target="_blank"
                 icon={<Icon name="open_in_new" size={IconSize.X_SMALL} />}
               ></Button>
@@ -187,4 +252,71 @@ export const ServiceBlock = ({
       </div>
     </div>
   );
+};
+
+const ServiceBlockEntitlements = (props: ServiceBlockProps) => {
+  return (
+    <div className="dc__service__block__entitlements">
+      {Object.entries(props.entitlementsFields).map(
+        ([entitlementType, entitlementFields]) => (
+          <ServiceBlockEntitlement
+            key={entitlementType}
+            entitlementName={entitlementType}
+            entitlementFields={entitlementFields}
+            {...props}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+type ServiceBlockEntitlementProps = {
+  entitlementName: string;
+  entitlementFields: EntitlementFields;
+} & ServiceBlockProps;
+
+const ServiceBlockEntitlement = (props: ServiceBlockEntitlementProps) => {
+  const entitlement = props.service.subscription?.entitlements?.find(
+    (e) => e.type === props.entitlementName
+  );
+  if (!entitlement) {
+    return null;
+  }
+  return (
+    <div className="dc__service__block__entitlement">
+      <div className="dc__service__block__entitlement__fields">
+        {Object.entries(props.entitlementFields.fields).map(
+          ([fieldName, field]) => {
+            return (
+              <ServiceBlockEntitlementField
+                key={fieldName}
+                fieldName={fieldName}
+                field={field}
+                entitlement={entitlement}
+                {...props}
+              />
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
+};
+
+export type ServiceBlockEntitlementFieldProps = {
+  fieldName: string;
+  field: { type: string };
+  entitlement: Entitlement;
+} & ServiceBlockEntitlementProps;
+
+const ServiceBlockEntitlementField = (
+  props: ServiceBlockEntitlementFieldProps
+) => {
+  switch (props.field.type) {
+    case "storage_picker":
+      return <StoragePickerEntitlementField {...props} />;
+    default:
+      return <div>Unknown field type: {props.field.type}</div>;
+  }
 };
---
+++ src/frontend/src/features/ui/components/service/entitlements/fields/StoragePickerEntitlementField.tsx
@@ -0,0 +1,218 @@
+import { ServiceBlockEntitlementFieldProps } from "@/features/ui/components/service/ServiceBlock";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalProps,
+  ModalSize,
+  Select,
+  useModal,
+} from "@openfun/cunningham-react";
+import { useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { ServiceAttribute } from "@/features/ui/components/service/ServiceAttribute";
+import { useMutationUpdateEntitlement } from "@/hooks/useQueries";
+import { useOperatorContext } from "@/features/layouts/components/GlobalLayout";
+import { Spinner } from "@gouvfr-lasuite/ui-kit";
+
+const DECIMALS = 2;
+// Order matters ! The biggest unit should be first.
+const UNITS = {
+  TB: 1000 * 1000 * 1000 * 1000,
+  GB: 1000 * 1000 * 1000,
+  MB: 1000 * 1000,
+};
+
+/**
+ * Gets the value and unit from a number of bytes.
+ * The number is formatted with the correct number of decimals.
+ * The unit is the biggest unit possible if the number is greater than 1, otherwise the smallest unit.
+ */
+const fromBytes = (bytes: number) => {
+  let out: [number, string] | undefined = undefined;
+  for (const unit in UNITS) {
+    const multiplier = UNITS[unit as keyof typeof UNITS];
+    if (bytes / multiplier >= 1) {
+      out = [bytes / multiplier, unit];
+      break;
+    }
+  }
+  if (!out) {
+    // If no unit is found, use the last unit.
+    const lastUnit = Object.keys(UNITS)[Object.keys(UNITS).length - 1];
+    const lastUnitMultiplier = UNITS[lastUnit as keyof typeof UNITS];
+    out = [bytes / lastUnitMultiplier, lastUnit];
+  }
+
+  // Format the number with the correct number of decimals.
+  out[0] = parseFloat(out[0].toFixed(DECIMALS));
+  return out;
+};
+
+/**
+ * Gets the number of bytes from a value and unit.
+ */
+const toBytes = (value: number, unit: string) => {
+  const multiplier = UNITS[unit as keyof typeof UNITS];
+  return value * multiplier;
+};
+
+/**
+ * Gets the translation prefix for an entitlement field.
+ */
+const getTranslationPrefix = (
+  serviceType: string,
+  entitlementName: string,
+  fieldName: string
+) => {
+  return `organizations.services.types.${serviceType}.entitlements.${entitlementName}.${fieldName}`;
+};
+
+export const StoragePickerEntitlementField = (
+  props: ServiceBlockEntitlementFieldProps
+) => {
+  const { t } = useTranslation();
+  const [value, unit] = useMemo(
+    () =>
+      fromBytes(parseInt(props.entitlement?.config.max_storage as string) || 0),
+    [props.entitlement?.config.max_storage]
+  );
+
+  const modal = useModal({
+    isOpenDefault: false,
+  });
+  const translationPrefix = getTranslationPrefix(
+    props.service.type,
+    props.entitlementName,
+    props.fieldName
+  );
+  return (
+    <>
+      {/* The goal is reset the modal state when the component is unmounted */}
+      {modal.isOpen && (
+        <StoragePickerEntitlementFieldModal {...modal} {...props} />
+      )}
+      <ServiceAttribute
+        name={t(`${translationPrefix}.label`)}
+        value={`${value} ${unit}`}
+        onClick={() => modal.open()}
+        interactive={true}
+      />
+    </>
+  );
+};
+
+const StoragePickerEntitlementFieldModal = (
+  props: ServiceBlockEntitlementFieldProps &
+    Pick<ModalProps, "isOpen" | "onClose">
+) => {
+  const translationPrefix = getTranslationPrefix(
+    props.service.type,
+    props.entitlementName,
+    props.fieldName
+  );
+  const { t } = useTranslation();
+
+  const [initialValue, initialUnit] = fromBytes(
+    parseInt(props.entitlement?.config.max_storage as string) || 0
+  );
+
+  const { operatorId } = useOperatorContext();
+  const { mutate: updateEntitlement } = useMutationUpdateEntitlement();
+
+  const [value, setValue] = useState(initialValue);
+  const [unit, setUnit] = useState(initialUnit);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    updateEntitlement(
+      {
+        operatorId,
+        organizationId: props.organization.id,
+        serviceId: props.service.id,
+        entitlementId: props.entitlement.id,
+        data: {
+          config: { max_storage: toBytes(value, unit) },
+        },
+      },
+      {
+        onSuccess: () => {
+          setIsLoading(false);
+          props.onClose();
+        },
+        onError: () => {
+          setIsLoading(false);
+        },
+      }
+    );
+  };
+
+  return (
+    <Modal
+      size={ModalSize.SMALL}
+      title={t(`${translationPrefix}.modal.title`)}
+      closeOnEsc={true}
+      closeOnClickOutside={true}
+      rightActions={
+        <>
+          <Button
+            type="button"
+            onClick={props.onClose}
+            color="secondary"
+            disabled={isLoading}
+          >
+            {t("common.cancel")}
+          </Button>
+          <Button
+            type="submit"
+            form="storage-picker-form"
+            disabled={isLoading}
+            icon={isLoading ? <Spinner /> : undefined}
+            iconPosition="right"
+          >
+            {t("common.save")}
+          </Button>
+        </>
+      }
+      {...props}
+    >
+      <div className="dc__service__attribute__modal__content">
+        <p className="dc__service__attribute__modal__content__help">
+          {t(`${translationPrefix}.modal.description`)}
+        </p>
+
+        <form
+          id="storage-picker-form"
+          onSubmit={submit}
+          className="dc__service__attribute__modal__content__storage-picker__inputs"
+        >
+          <Input
+            label={t(
+              `organizations.services.entitlements.fields.storage_picker.input_placeholder`
+            )}
+            type="number"
+            min="0"
+            step="0.01"
+            value={value}
+            onChange={(e) => setValue(parseFloat(e.target.value))}
+          />
+          <Select
+            label={t(
+              `organizations.services.entitlements.fields.storage_picker.unit_placeholder`
+            )}
+            value={unit}
+            clearable={false}
+            onChange={(e) => setUnit(e.target.value as string)}
+            options={Object.keys(UNITS).map((unit) => ({
+              label: unit,
+              value: unit,
+            }))}
+          />
+        </form>
+      </div>
+    </Modal>
+  );
+};
