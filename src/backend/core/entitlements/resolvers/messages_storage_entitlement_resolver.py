55a762a3b20f6965c622a2e54bfd8260bafb035b
Nathan Vasse <nathan.vasse@gmail.com>
2025-12-12 18:13:13 +0100

    wip

--- src/backend/core/entitlements/resolvers/messages_storage_entitlement_resolver.py
+++ src/backend/core/entitlements/resolvers/messages_storage_entitlement_resolver.py
@@ -3,19 +3,11 @@ from core import models
 
 def order_entitlements_by_priority(entitlements):
     """
-    This orders entitlements by this order of priority (the first one is the highest priority):
-    (account_type="user", account_id="xyz)
-    (account_type="organization", account_id=None)
-    (account_type="user", account_id=None)
-
-    Basically in real terms it translations to this:
-    Your entitlement should comply to (this first one is the highest priority):
-    - An entitlement for a specific user - can be considered as an override (account_type="user", account_id="xyz")
-    - An entitlement for organization (account_type="organization", account_id=None)
-    - An entitlement for user (account_type="user", account_id=None)
-
-    There is no entitlement for a specific organization because as an entitlement is
-    already bounds to an organization via the service subscription.
+    TODO: IMPLEMENT 
+    It sort entitlements by their hierarchy:
+    user_override = (account_type="user", account_id="xyz)
+    organization = (account_type="organization", account_id=None)
+    user = (account_type="user", account_id=None)
     """
     return {
         user_override: None,
@@ -30,8 +22,84 @@ class MessagesStorageEntitlementResolver:
     Messages storage entitlement resolver.
     """
 
-    reason_prefix = ""
+    resolve_level_prefix = ""
 
+    def resolve(self, context):
+        """
+        Resolve the messages storage entitlement based on their priority.
+
+        Basically in real terms it translates to:
+        Your entitlement should comply to (this first one is the highest priority):
+        - An entitlement for a specific user - can be considered as an blocking override (account_type="user", account_id="xyz")
+        - An entitlement for organization (account_type="organization", account_id=None)
+        - An entitlement for user (account_type="user", account_id=None)
+
+        There is no entitlement for a specific organization (account_type="organization", account_id="xyz")
+        because as an entitlement is already bounds to an organization via the service subscription. it would
+        make no sense.
+
+        Example:
+        We have a storage entitlement on drive that tells if a user can store new files.
+        In order to be able to store new file it goes like:
+        - If there is a user override (account_type="user", account_id="xyz"), 
+            we only consider this one. Whether it lowers or raises its storage compared 
+            to the default user or organization entitlement. It is an override.
+        - If there is an organization entitlement (account_type="organization", account_id=None), 
+            we should make sure the entire organization have free storage left. If not, 
+            we should return false immediately.
+        - We can now check the default user entitlement (account_type="user", account_id=None).
+            At this point, there is no user override, and if there is an organization entitlement
+            it tells that the organization still have free storage left. So we can finally check
+            that the user has free storage left for the default user entitlement.
+
+        This method also return in the object the level at which the entitlement was resolved or not via
+        "_reason_level" key.
+
+        Example:
+        {
+            "can_store": True,
+            "can_store_resolve_level": "user_override",
+        }
+        """
+        
+
+        entitlements = order_entitlements_by_priority(context["entitlements"])
+        entitlement_user = entitlements.get("user")
+
+        # The user override is the highest priority entitlement. Whether it
+        # resolves or not, we will return the result.
+        if entitlement_user_override := entitlements.get("user_override"):
+            metric = self._get_metric(context, entitlement_user_override)
+            _, attributes = self.resolve_entitlement(context, entitlement_user_override, metric)
+            return self._build_resolve_level(attributes, "user_override")
+
+        # If there is an organization entitlement, it should first resolve anyway
+        # before the generic user entitlement.
+        if entitlement_organization := entitlements.get("organization"):
+            metric = self._get_metric(context, entitlement_organization)
+            resolves, attributes = self.resolve_entitlement(context, entitlement_organization, metric)
+            # If there is a user entitlement and it this one does not resolve, we can return directly.
+            # Or if there is no user entitlement to run further, we can return directly in anyway.
+            if not resolves or not entitlement_user:
+                return self._build_resolve_level(attributes, "organization")
+
+        # If there is a generic user entitlement, it should be the last one to resolve.
+        if entitlement_user:
+            metric = self._get_metric(context, entitlement_user)
+            resolves, attributes = self.resolve_entitlement(context, entitlement_user, metric)
+            return self._build_resolve_level(attributes, "user")
+
+        raise ValueError(
+            f"This should not happen, no user or organization entitlement could be resolved for the given context. "
+            f"Service {context['service'].name}, organization {context['organization'].name}, "
+            f"account type {context['account_type']}, account id {context['account_id']}"
+        )
+
+    def _build_resolve_level(self, attributes, resolve_level):
+        """
+        Build the resolve level string.
+        """
+        return {**attributes, f"{self.resolve_level_prefix}_resolve_level": resolve_level}
 
     def _get_metric(self, context, entitlement):
         """
@@ -59,56 +127,7 @@ class MessagesStorageEntitlementResolver:
             .first()
         )
 
-        return metric
-
-    def resolve(self, context):
-        """
-        Resolve the messages storage entitlement.
-
-        Example:
-        We have a storage entitlement on drive that tells if a user can store new files.
-        In order to be able to store new file it goes like:
-        - If there is a user override (account_type="user", account_id="xyz"), 
-            we only consider this one. Whether it lowers or raises its storage compared 
-            to the default user or organization entitlement. It is an override.
-        - If there is an organization entitlement (account_type="organization", account_id=None), 
-            we should make sure the entire organization have free storage left. If not, 
-            we should return false immediately.
-        - We can now check the default user entitlement (account_type="user", account_id=None).
-            At this point, there is no user override, and if there is an organization entitlement
-            it tells that the organization still have free storage left. So we can finally check
-            that the user has free storage left for the default user entitlement.
-        """
-        
-
-        entitlements = order_entitlements_by_priority(context["entitlements"])
-
-        # The user override is the highest priority entitlement. Whether it
-        # resolves or not, we will return the result.
-        if entitlement_user_override := entitlements.get("user_override"):
-            metric = self._get_metric(context, entitlement_user_override)
-            _, attributes = self.resolve_entitlement(context, entitlement_user_override, metric)
-            return {**attributes, f"{self.reason_prefix}_reason_level": "user_override"}
-
-        # If there is an organization entitlement, it should first resolve anyway
-        # before the generic user entitlement.
-        if entitlement_organization := entitlements.get("organization"):
-            metric = self._get_metric(context, entitlement_organization)
-            resolves, attributes = self.resolve_entitlement(context, entitlement_organization, metric)
-            if not resolves:
-                return {**attributes, f"{self.reason_prefix}_reason_level": "organization"}
-
-        # If there is a generic user entitlement, it should be the last one to resolve.
-        if entitlement_user := entitlements.get("user"):
-            metric = self._get_metric(context, entitlement_user)
-            resolves, attributes = self.resolve_entitlement(context, entitlement_user, metric)
-            return {**attributes, f"{self.reason_prefix}_reason_level": "user"}
-
-        raise ValueError(
-            f"This should not happen, no user or organization entitlement could be resolved for the given context. "
-            f"Service {context['service'].name}, organization {context['organization'].name}, "
-            f"account type {context['account_type']}, account id {context['account_id']}"
-        )
+        return metric        
 
     # Child method, abstract.
     def resolve_entitlement(self, context, entitlement, metric):
