e56d6846057f347a9e5fd234202ec35ac28ce935
Nathan Vasse <nathan.vasse@gmail.com>
2025-12-19 14:31:23 +0100

    ✨(back) adapt entitlements routes to hierarchy
    This route needs some upgrade in order to fit with the new
    hierarchy of entitlements. It now needs to know if it needs
    to scrape the usage metric on the organization or not based
    on the defined entitlements priorities.

--- src/backend/core/api/viewsets/entitlements.py
+++ src/backend/core/api/viewsets/entitlements.py
@@ -14,6 +14,9 @@ from core.entitlements.resolvers import get_entitlement_resolver
 from core.entitlements.resolvers.access_entitlement_resolver import (
     AccessEntitlementResolver,
 )
+from core.entitlements.resolvers.entitlement_resolver import (
+    get_entitlements_by_priority,
+)
 from core.tasks.metrics import scrape_service_usage_metrics
 
 
@@ -46,9 +49,7 @@ class EntitlementView(APIView):
         serializer.is_valid(raise_exception=True)
         siret = serializer.validated_data["siret"]
 
-        organization = models.Organization.objects.filter(
-            siret=siret
-        ).first()
+        organization = models.Organization.objects.filter(siret=siret).first()
 
         service = models.Service.objects.filter(
             id=serializer.validated_data["service_id"]
@@ -83,21 +84,53 @@ class EntitlementView(APIView):
         entitlements_data = {**AccessEntitlementResolver().resolve(entitlement_context)}
 
         if service_subscription and service_subscription.is_active:
-            scrape_service_usage_metrics(
-                service,
-                {
-                    "account_type": account_type,
-                    "account_id": account_id,
-                },
-            )
             operator_data = OperatorSerializer(service_subscription.operator).data
             entitlements = models.Entitlement.objects.filter(
                 service_subscription=service_subscription
             )
+            entitlements_by_type = {}
             for entitlement in entitlements:
-                resolver = get_entitlement_resolver(entitlement.type)
+                if entitlement.type not in entitlements_by_type:
+                    entitlements_by_type[entitlement.type] = []
+                entitlements_by_type[entitlement.type].append(entitlement)
+
+            # Always scrape user metrics.
+            scrape_user = True
+            # Not all services supports organization account type.
+            scrape_organization = False
+
+            # Determine if we need to scrape organization metrics.
+            # We scrape organization metrics only if we at least one organization entitlement.
+            for entitlement_type, entitlements_of_type in entitlements_by_type.items():
+                entitlements_by_priority = get_entitlements_by_priority(
+                    entitlements_of_type
+                )
+                if entitlements_by_priority.get("organization"):
+                    scrape_organization = True
+
+            # Scrape metrics.
+            if scrape_user:
+                scrape_service_usage_metrics(
+                    service,
+                    {
+                        "account_type": account_type,
+                        "account_id": account_id,
+                    },
+                )
+            if scrape_organization:
+                scrape_service_usage_metrics(
+                    service,
+                    {
+                        "account_type": "organization",
+                        "account_id": organization.id,
+                    },
+                )
+
+            # Resolve entitlements.
+            for entitlement_type, entitlements_of_type in entitlements_by_type.items():
+                resolver = get_entitlement_resolver(entitlement_type)
                 entitlement_data = resolver.resolve(
-                    {**entitlement_context, "entitlement": entitlement}
+                    {**entitlement_context, "entitlements": entitlements_of_type}
                 )
                 entitlements_data = {**entitlements_data, **entitlement_data}
 
